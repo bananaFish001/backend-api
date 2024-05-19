76f3a54de4d8b4588acbc8b5d9bed1cb67604c98
bananaFish001 <raj123.rs308@gmail.com>
2024-05-19 19:51:55 +0530

    exporting the modules

--- node_practice/index.js
+++ node_practice/index.js
@@ -1,3 +1,9 @@
-require('./add.js')
+const addfn = require('./add.js')
 
-console.log("With love from Russia")
+console.log("With love from Russia");
+
+const sum1 = addfn(5, 7);
+const sum2 = addfn(10, 11);
+
+console.log(sum1)
+console.log(sum2)
