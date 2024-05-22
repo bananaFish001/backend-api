7ce347ccdf517ea5fffa8172ebd175397f63d216
bananaFish001 <raj123.rs308@gmail.com>
2024-05-22 23:36:02 +0530

    starting the server

--- node-api/server.js
+++ node-api/server.js
@@ -1,1 +1,11 @@
-console.log('hello world')
+const express = require('express');
+const app = express();
+
+// Routes
+app.get('/', (req, res) => {
+    
+})
+
+app.listen(3000, () => {
+    console.log(`Node API app cis running on port 3000   `)
+})
