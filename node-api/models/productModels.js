583be2e1607209c5c38dfb1f2d0ed8ea3f6392b3
bananaFish001 <raj123.rs308@gmail.com>
2024-05-25 23:13:27 +0530

    establishing the mongodb database

--- node-api/models/productModels.js
+++ node-api/models/productModels.js
@@ -5,6 +5,15 @@ const productSchema = mongoose.Schema(
         name: {
             type: String,
             required: [true, "Please enter a product name"]
+        },
+        quantity: {
+            type: Number, 
+            required: true, 
+            default: 0
+        },
+        image: {
+            type: String,
+            required: false
         }
     }
 )
